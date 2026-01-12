42e02c41100db724d8dbb11f42ea3627d8fb4a7f
Anshul Koshyari <anshulkoshyari@gmail.com>
2026-01-12 18:11:16 +0530

    Added deploy script

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://your-domain.com/sitemap.xml",
+    sitemap: "https://koshyari.com/sitemap.xml",
   };
 }
 
